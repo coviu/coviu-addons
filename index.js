dec1a99cd795f66904a9ff0ef8bdbd7afb64518d
Suresh Ponraj <sureshponraj@MacBook-Pro.local>
2021-02-01 11:47:29 +1000

    SP Coviu Demo Plugin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const drawer = require('./lib/drawer')
-const view = require('./lib/view')
+const drawer = require('./lib/drawer');
+const precallView = require('./lib/precallView');
+const postcallView = require('./lib/postcallView');
 const pluginConfig = require('./lib/configure');
-
 
 /**
  * Plugin instantiation method
@@ -9,14 +9,18 @@ const pluginConfig = require('./lib/configure');
  */
 function plugin(api) {
 	return Promise.all([
-		// Registers a view
-		api.views.register(view(api)),
+		// Registers a view precall view
+		api.views.register(precallView(api)),
+
+		// Registers a view postcall view
+		api.views.register(postcallView(api)),
+
 		// Register the drawer. You could programmatically choose to register the drawer only for a particular role
 		// of person
 		api.drawers.registerDrawer(drawer(api))
 	]).then(function() {
 		return {
-			name: 'View and Drawer Example'
+			name: 'Coviu Demo Plugin'
 		};
 	});
 }
