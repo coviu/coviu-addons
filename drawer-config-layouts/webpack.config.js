65d97f23a661ff097c38b33a3fb1f26e3be4217b
Rohan Richards <scenes@quartermoondesigns.com>
2022-01-12 16:09:53 +1000

    remove postcss

--- drawer-config-layouts/webpack.config.js
+++ drawer-config-layouts/webpack.config.js
@@ -1,50 +1,47 @@
-const webpack = require('webpack');
+import { EnvironmentPlugin } from 'webpack';
 
-module.exports = {
-  entry: {
-    plugin: './index.js',
+export const entry = {
+  plugin: './index.js',
+};
+export const output = {
+  path: __dirname + '/dist',
+  filename: '[name].js',
+};
+export const module = {
+  rules: [
+    {
+      /* Only run Babel on our local JS, not our dependencies */
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    },
+    {
+      test: /\.module\.css$/i,
+      use: [
+        'style-loader', 'css-loader'
+      ],
+    },
+  ],
+};
+export const plugins = [new EnvironmentPlugin({ NODE_ENV: 'development' })];
+export const devtool = 'hidden-source-map';
+export const devServer = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
   },
-  output: {
-    path: __dirname + '/dist',
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        /* Only run Babel on our local JS, not our dependencies */
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      },
-      {
-        test: /\.module\.css$/i,
-        use: [
-          'style-loader','css-loader'
-        ],
-      },
-    ],
-  },
-  plugins: [new webpack.EnvironmentPlugin({NODE_ENV: 'development'})],
-  devtool: 'hidden-source-map',
-  devServer: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers':
-        'X-Requested-With, content-type, Authorization',
-    },    
-    port: 9100,
-    host: 'localhost',
-    hot: false,
-    static: './dist',
-    watchFiles: ['./index.js', './lib/**/*'],
-    allowedHosts: ['.dev.localhost', '.localhost'],
-  },
-  watchOptions: {
-    aggregateTimeout: 100,
-    poll: true,
-  },
+  port: 9100,
+  host: 'localhost',
+  hot: false,
+  static: './dist',
+  watchFiles: ['./index.js', './lib/**/*'],
+  allowedHosts: ['.dev.localhost', '.localhost'],
+};
+export const watchOptions = {
+  aggregateTimeout: 100,
+  poll: true,
 };
