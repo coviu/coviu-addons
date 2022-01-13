acac7c3a379c0bcbcc9bac3b2628755e4fe52e81
Rohan Richards <scenes@quartermoondesigns.com>
2022-01-13 10:33:09 +1000

    reverting webpack
    accidentally made webpack an es6 module so reverting

--- drawer-config-layouts/webpack.config.js
+++ drawer-config-layouts/webpack.config.js
@@ -1,47 +1,50 @@
-import { EnvironmentPlugin } from 'webpack';
+const webpack = require('webpack');
 
-export const entry = {
-  plugin: './index.js',
-};
-export const output = {
-  path: __dirname + '/dist',
-  filename: '[name].js',
-};
-export const module = {
-  rules: [
-    {
-      /* Only run Babel on our local JS, not our dependencies */
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env']
-      }
-    },
-    {
-      test: /\.module\.css$/i,
-      use: [
-        'style-loader', 'css-loader'
-      ],
-    },
-  ],
-};
-export const plugins = [new EnvironmentPlugin({ NODE_ENV: 'development' })];
-export const devtool = 'hidden-source-map';
-export const devServer = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+module.exports = {
+  entry: {
+    plugin: './index.js',
   },
-  port: 9100,
-  host: 'localhost',
-  hot: false,
-  static: './dist',
-  watchFiles: ['./index.js', './lib/**/*'],
-  allowedHosts: ['.dev.localhost', '.localhost'],
-};
-export const watchOptions = {
-  aggregateTimeout: 100,
-  poll: true,
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        /* Only run Babel on our local JS, not our dependencies */
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader', 'css-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.EnvironmentPlugin({NODE_ENV: 'development'})],
+  devtool: 'hidden-source-map',
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
+    port: 9100,
+    host: 'localhost',
+    hot: false,
+    static: './dist',
+    watchFiles: ['./index.js', './lib/**/*'],
+    allowedHosts: ['.localhost'],
+  },
+  watchOptions: {
+    aggregateTimeout: 100,
+    poll: true,
+  },
 };
