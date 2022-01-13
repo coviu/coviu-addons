e8c9dafd4d3cbdf882bc287382cb1707ef2caa85
Rohan Richards <scenes@quartermoondesigns.com>
2022-01-13 10:43:09 +1000

    modules and webpack
    updated the webpack and converted to es6 modules

--- precall-postcall-views/lib/preCallView.js
+++ precall-postcall-views/lib/preCallView.js
@@ -1,9 +1,9 @@
-const styles = require('./styles/view.module.scss');
+import styles from './styles/view.module.css';
 
 /**
   This is an example precall flow view
  **/
-module.exports = (api) => {
+export default function(api) {
 
 	const { value } = api.observ;
 	const { h } = api.render;
